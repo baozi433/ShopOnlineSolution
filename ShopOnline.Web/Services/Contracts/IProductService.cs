a711b6d78a1f679225b76d87287c9fa7bc75a766
Yuanjie Wu <yuanjie.wu@canterbury.ac.nz>
2022-05-06 10:25:53 +1200

    Products Razor page
    Update the razor page

---
+++ ShopOnline.Web/Services/Contracts/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using ShopOnlie.Models.Dtos;
+
+namespace ShopOnline.Web.Services.Contracts
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDto>> GetItems();
+
+    }
+}
